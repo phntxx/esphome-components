a5eef3330dba1a0c75d11c95138390259950d8d0
Bastian <bastian@phntxx.com>
2024-10-07 11:18:33 +0200

    update loctek

--- components/loctek/loctek.cpp
+++ components/loctek/loctek.cpp
@@ -10,49 +10,109 @@ namespace esphome
 
     static const char *const TAG = "loctek";
 
+    void LoctekComponent::setup()
+    {
+      // Publish initial value
+      this->height_->publish_state(this->height_val_);
+    }
+
     void LoctekComponent::loop()
     {
       while (available() > 0)
       {
         byte incomingByte = read();
+        // ESP_LOGD("DEBUG", "Incoming byte is: %08x", incomingByte);
 
-        // Handle first byte: start of a packet
+        // First byte, start of a packet
         if (incomingByte == 0x9b)
         {
+          // Reset message length
           this->msg_len_ = 0;
           this->valid_ = false;
         }
 
-        // Update history buffer
-        this->update_history_(incomingByte);
-
-        // Parse packet based on history state
+        // Second byte defines the message length
         if (this->history_[0] == 0x9b)
         {
-          this->handle_packet_start_(incomingByte);
-        }
-        else if (this->history_[1] == 0x9b)
-        {
-          this->handle_packet_length_(incomingByte);
-        }
-        else if (this->history_[2] == 0x9b)
-        {
-          this->handle_message_type_(incomingByte);
-        }
-        else if (this->history_[3] == 0x9b)
-        {
-          this->handle_first_height_digit_(incomingByte);
-        }
-        else if (this->history_[4] == 0x9b && this->valid_)
-        {
-          this->calculate_height_(incomingByte);
+          this->msg_len_ = (int)incomingByte;
         }
 
-        // Handle end byte
-        if (incomingByte == 0x9d && this->height_val_ && this->height_val_ != this->lastPublished_)
+        // Third byte is message type
+        if (this->history_[1] == 0x9b)
         {
-          this->height_->publish_state(this->height_val_);
-          this->lastPublished_ = this->height_val_;
+          this->msg_type_ = incomingByte;
+        }
+
+        // Fourth byte is first height digit, if msg type 0x12 & msg len 7
+        if (this->history_[2] == 0x9b)
+        {
+
+          if (this->msg_type_ == 0x12 && (this->msg_len_ == 7 || this->msg_len_ == 10))
+          {
+            // Empty height
+            if (incomingByte == 0)
+            {
+              // ESP_LOGD("DEBUG", "Height 1 is EMPTY -> 0x%02x", incomingByte);
+              // deskSerial.write(command_wakeup, sizeof(command_wakeup));
+            }
+            else if (this->hey_to_int_(incomingByte) == 0)
+            {
+              // ESP_LOGD("DEBUG", "Invalid height 1 -> 0x%02x", incomingByte);
+              // deskSerial.write(command_wakeup, sizeof(command_wakeup));
+            }
+            else
+            {
+              this->valid_ = true;
+              //   ESP_LOGD("DEBUG", "Height 1 is: 0x%02x", incomingByte);
+            }
+          }
+        }
+
+        // Fifth byte is second height digit
+        if (this->history_[3] == 0x9b)
+        {
+          if (this->valid_ == true)
+          {
+            // ESP_LOGD("DEBUG", "Height 2 is: 0x%02x", incomingByte);
+          }
+        }
+
+        // Sixth byte is third height digit
+        if (this->history_[4] == 0x9b)
+        {
+          if (this->valid_ == true)
+          {
+            int height1 = this->hey_to_int_(this->history_[1]) * 100;
+            int height2 = this->hey_to_int_(this->history_[0]) * 10;
+            int height3 = this->hey_to_int_(incomingByte);
+            if (height2 != 100)
+            {
+              float finalHeight = height1 + height2 + height3;
+              if (this->is_decimal_(this->history_[0]))
+              {
+                finalHeight = finalHeight / 10;
+              }
+              this->height_val_ = finalHeight;
+              // ESP_LOGD("DeskHeightSensor", "Current height is: %f", finalHeight);
+            }
+          }
+        }
+
+        // Save byte buffer to history arrary
+        this->history_[4] = this->history_[3];
+        this->history_[3] = this->history_[2];
+        this->history_[2] = this->history_[1];
+        this->history_[1] = this->history_[0];
+        this->history_[0] = incomingByte;
+
+        // End byte
+        if (incomingByte == 0x9d)
+        {
+          if (this->height_val_ && this->height_val_ != this->lastPublished_)
+          {
+            this->height_->publish_state(this->height_val_);
+            this->lastPublished_ = this->height_val_;
+          }
         }
       }
     }
@@ -72,118 +132,60 @@ namespace esphome
       this->check_uart_settings(9600);
     }
 
-    // Helper functions
-    void LoctekComponent::update_history_(byte b)
-    {
-      for (int i = 4; i > 0; --i)
-      {
-        this->history_[i] = this->history_[i - 1];
-      }
-      this->history_[0] = b;
-    }
-
-    void LoctekComponent::handle_packet_start_(byte b)
-    {
-      // Set message length if the first byte is 0x9b
-      this->msg_len_ = static_cast<int>(b);
-    }
-
-    void LoctekComponent::handle_packet_length_(byte b)
-    {
-      this->msg_type_ = b;
-    }
-
-    void LoctekComponent::handle_message_type_(byte b)
-    {
-      if (this->msg_type_ == 0x12 && (this->msg_len_ == 7 || this->msg_len_ == 10))
-      {
-        if (b == 0 || this->hex_to_int_(b) == 0)
-        {
-          // Invalid height, handle accordingly (logging or command wakeup could be added here)
-        }
-        else
-        {
-          this->valid_ = true;
-        }
-      }
-    }
-
-    void LoctekComponent::handle_first_height_digit_(byte b)
-    {
-      if (this->valid_)
-      {
-        // Height digit 2 can be processed here (optional logging)
-      }
-    }
-
-    void LoctekComponent::calculate_height_(byte b)
-    {
-      int height1 = this->hex_to_int_(this->history_[1]) * 100;
-      int height2 = this->hex_to_int_(this->history_[0]) * 10;
-      int height3 = this->hex_to_int_(b);
-
-      if (height2 != 100)
-      {
-        float finalHeight = height1 + height2 + height3;
-        if (this->is_decimal_(this->history_[0]))
-        {
-          finalHeight /= 10;
-        }
-        this->height_val_ = finalHeight;
-      }
-    }
-
     bool LoctekComponent::is_decimal_(byte b)
     {
       return (b & 0x80) == 0x80;
     }
 
-    int LoctekComponent::hex_to_int_(byte s)
+    int LoctekComponent::hey_to_int_(byte s)
     {
+      std::bitset<8> b(s);
 
-      /*
-      This method converts the hexadecimal value provided from the Loctek control box
-      to actual integers.
-
-      The loctek control box sends the direct signals that would normally drive a 7-segment display,
-      so the bits here correspond to the following segments:
-
-      -- 00 --
-      05 -- 01
-      05 -- 01
-      -- 06 --
-      04 -- 02
-      04 -- 02
-      -- 03 -- 07
-      */
-
-      switch (s)
+      if (b[0] && b[1] && b[2] && b[3] && b[4] && b[5] && !b[6])
       {
-      case 0x7E:
-        return 0;
-      case 0x06:
-        return 1;
-      case 0x6D:
-        return 2;
-      case 0x79:
-        return 3;
-      case 0x33:
-        return 4;
-      case 0x5B:
-        return 5;
-      case 0x5F:
-        return 6;
-      case 0x70:
-        return 7;
-      case 0x7F:
-        return 8;
-      case 0x7B:
-        return 9;
-      case 0x01:
-        return 10;
-      default:
         return 0;
       }
+      if (not b[0] && b[1] && b[2] && !b[3] && !b[4] && !b[5] && !b[6])
+      {
+        return 1;
+      }
+      if (b[0] && b[1] && !b[2] && b[3] && b[4] && !b[5] && b[6])
+      {
+        return 2;
+      }
+      if (b[0] && b[1] && b[2] && b[3] && !b[4] && !b[5] && b[6])
+      {
+        return 3;
+      }
+      if (not b[0] && b[1] && b[2] && !b[3] && !b[4] && b[5] && b[6])
+      {
+        return 4;
+      }
+      if (b[0] && !b[1] && b[2] && b[3] && !b[4] && b[5] && b[6])
+      {
+        return 5;
+      }
+      if (b[0] && !b[1] && b[2] && b[3] && b[4] && b[5] && b[6])
+      {
+        return 6;
+      }
+      if (b[0] && b[1] && b[2] && !b[3] && !b[4] && !b[5] && !b[6])
+      {
+        return 7;
+      }
+      if (b[0] && b[1] && b[2] && b[3] && b[4] && b[5] && b[6])
+      {
+        return 8;
+      }
+      if (b[0] && b[1] && b[2] && b[3] && !b[4] && b[5] && b[6])
+      {
+        return 9;
+      }
+      if (!b[0] && !b[1] && !b[2] && !b[3] && !b[4] && !b[5] && b[6])
+      {
+        return 10;
+      }
+      return 0;
     }
 
   } // namespace loctek
--- components/loctek/loctek.h
+++ components/loctek/loctek.h
@@ -14,16 +14,16 @@ namespace esphome
     {
     public:
       LoctekComponent() = default;
+      void set_height(sensor::Sensor *height) { height_ = height; }
 
+      void setup() override;
       void loop() override;
       void dump_config() override;
-      void setup() override {};
 
-      void set_height(sensor::Sensor *height) { height_ = height; }
       float get_setup_priority() const override { return setup_priority::DATA; }
 
     protected:
-      float height_val_ = 0;
+      float height_val_ = 0.0;
       float lastPublished_ = -1;
       unsigned long history_[5];
 
@@ -33,14 +33,8 @@ namespace esphome
 
       sensor::Sensor *height_{nullptr};
 
-      int hex_to_int_(byte b);
+      int hex_to_int_(byte s);
       bool is_decimal_(byte b);
-      void update_history_(byte b);
-      void handle_packet_start_(byte b);
-      void handle_packet_length_(byte b);
-      void handle_message_type_(byte b);
-      void handle_first_height_digit_(byte b);
-      void calculate_height_(byte b);
     };
 
   } // namespace loctek
